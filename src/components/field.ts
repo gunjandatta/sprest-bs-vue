4b1da898cd550b5aedcd703041206bb4170ea19a
Gunjan Datta <me@dattabase.com>
2020-07-11 13:27:40 -0400

    Updated the base template.

--- src/components/field.ts
+++ src/components/field.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import { Components } from "gd-sprest-bs";
-import { Base } from "gd-bs-vue/src/components/base";
+import Base from "gd-bs-vue/src/components/base.vue";
 
 export const Field = Vue.extend({
   name: "Field",
